fe8e5e2696ae6e604307bf81c4a9a70f08321399
Pri1s <pribiswal7@gmail.com>
2024-03-01 00:11:59 -0600

    Create Environment.server.lua

---
+++ src/server/Environment.server.lua
@@ -0,0 +1,11 @@
+local spinCamera = workspace.CameraAngles.Spin.Camera
+local centerObject = workspace.CameraAngles.Center  -- Replace with the actual name of the object to circle
+local radius = 75  -- Radius of the circular path
+local angularSpeed = 0.1  -- Speed of rotation (radians per second)
+
+while task.wait() do
+	local angle = time() * angularSpeed
+	local x = centerObject.Position.X + radius * math.cos(angle)
+	local z = centerObject.Position.Z + radius * math.sin(angle)  -- Maintain original Y height
+	spinCamera.CFrame = CFrame.new(x, part.Position.Y, z)  -- Set CFrame relative to center object
+end
